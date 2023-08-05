5b35c01a276025b8c9606aebd37f7014a7844220
Saikat <saikat25112000@gmail.com>
2023-08-06 00:57:23 +0600

    first commit

---
+++ practice.js
@@ -0,0 +1,12 @@
+function barbieken(word) {
+  var result = word.length;
+  if(result % 2 === 0){
+    console.log("Hi Berbie!");
+  }
+  else{
+    console.log("Hi Kane!");
+  }
+}
+const word = "pheroqs";
+const check = barbieken(word);
+
